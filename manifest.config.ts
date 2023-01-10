a0500155ff41c2cd142cf22aca74c57cb6c492c1
memory-lovers <8280057+memory-lovers@users.noreply.github.com>
2023-01-10 17:34:53 +0900

    add manifest.config.ts and crxjs plugin

---
+++ manifest.config.ts
@@ -0,0 +1,11 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+
+export const manifest = defineManifest({
+  manifest_version: 3,
+  name: "chrome-extension-sample",
+  description: "chrome-extension-sample",
+  version: "0.0.1",
+  action: {
+    default_popup: "index.html",
+  },
+});
