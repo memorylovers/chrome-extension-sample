463812f1553c46a7f344e3a5b510cad718df47a3
memory-lovers <8280057+memory-lovers@users.noreply.github.com>
2023-01-13 10:14:43 +0900

    change build option by mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,19 @@ import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import { manifest } from "./manifest.config";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), crx({ manifest })],
-});
+// https://ja.vitejs.dev/guide/env-and-mode.html#modes
+// mode = "development" | "production"
+export default ({ mode }) => {
+  const isProd = mode === "production";
+  return defineConfig({
+    plugins: [vue(), crx({ manifest })],
+    build: {
+      // 開発時はminifyしない
+      minify: isProd,
+    },
+    esbuild: {
+      // 本番時はconsoleを削除する
+      drop: isProd ? ["console"] : undefined,
+    },
+  });
+};
