5b351dd3ee64c9e5c187e458736478653e7ed868
memory-lovers <8280057+memory-lovers@users.noreply.github.com>
2023-01-13 10:22:48 +0900

    use package.json in manifest.config.ts

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,10 +1,11 @@
 import { defineManifest } from "@crxjs/vite-plugin";
+import pkg from "./package.json";
 
 export const manifest = defineManifest({
   manifest_version: 3,
-  name: "chrome-extension-sample",
-  description: "chrome-extension-sample",
-  version: "0.0.1",
+  name: pkg.name,
+  description: pkg.description,
+  version: pkg.version,
   icons: {
     "16": "icons/icon_16.png",
     "32": "icons/icon_32.png",
